320dc185519c0a623a453eedac42e6f998a21b30
Ouail-01 <ouaillaarabi@gmail.com>
2023-03-06 14:42:43 +0100

    Basic styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Nav from './Nav';
 import logo from '../Images/logo.png';
+import './Header.css';
 
 const Header = () => (
-  <div className="header-logo">
+  <div className="header-logo flex">
     <div className="main-header">
       <img src={logo} alt="logo" />
       <h1>Space Traveler&apos;s Hub</h1>
