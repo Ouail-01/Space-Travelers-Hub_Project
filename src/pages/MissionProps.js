8377662426c75b2bd02c0866c6199af38be9ad13
Ouail-01 <ouaillaarabi@gmail.com>
2023-03-07 10:59:36 +0100

    Implement the button action

--- src/pages/MissionProps.js
+++ src/pages/MissionProps.js
@@ -1,17 +1,33 @@
+/* eslint-disable */
 import React from 'react';
 import PropTypes from 'prop-types';
+import { joinMission, leaveMission } from '../redux/missions/missionSlice';
+import { useDispatch } from 'react-redux';
 
 const MissionProps = (props) => {
   const {
-    id, missionName, description,
+    id, missionName, description, reserved,
   } = props;
+
+  const dispatch = useDispatch();
+  const join = () => {
+    dispatch(joinMission(id));
+  }
+  const leave = () => {
+    dispatch(leaveMission(id));
+  }
 
   return (
     <tr id={id}>
       <td>{missionName}</td>
       <td>{description}</td>
-      <td><p>NOT A MEMBER</p></td>
-      <td><button type="button">Join Mission</button></td>
+      <td><p>{reserved ? 'ACTIVE MEMBER' : 'NOT A MEMBER'}</p></td>
+      <td>
+        {!reserved ?
+        <button type="button" onClick={join}>Join Mission</button>
+        :<button type="button" onClick={leave}>Leave Mission</button>
+        }
+      </td>
     </tr>
   );
 };
@@ -20,6 +36,7 @@ MissionProps.propTypes = {
   id: PropTypes.string.isRequired,
   missionName: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
+  reserved: PropTypes.bool.isRequired,
 };
 
 export default MissionProps;
