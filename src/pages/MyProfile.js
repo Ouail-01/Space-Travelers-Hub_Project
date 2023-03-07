2934d9be4c3f98688d806ad7badd9ba1df5bc891
Joshua Lyngdoh <joshualyngdoh52@gmail.com>
2023-03-07 19:24:16 +0530

    Changes to src directory

--- src/pages/MyProfile.js
+++ src/pages/MyProfile.js
@@ -1,7 +1,37 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-const MyProfile = () => (
-  <div>MyProfile</div>
-);
+function MyProfile() {
+  const { missions } = useSelector((state) => state.missions);
+  const reservedMissions = missions.filter((item) => item.reserved === true);
+
+  const rockets = useSelector((state) => state.rockets);
+  const reservedRockets = rockets.filter((item) => item.reserved === true);
+
+  if (reservedMissions.length === 0 && reservedRockets.length === 0) {
+    return <div>No reserved rockets/missions</div>;
+  }
+
+  return (
+    <div>
+      <div>
+        <h2>My Missions</h2>
+        <ul>
+          {reservedMissions.map((item) => (
+            <li key={item.id}>{item.name}</li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <h2>My Rockets</h2>
+        <ul>
+          {reservedRockets.map((rocket) => (
+            <li key={rocket.id}>{rocket.name}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
 
 export default MyProfile;
